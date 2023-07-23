2d4f7bcb4ec03abe0f49e1977b17edda1c439f78
abhinav jha <116362054+momomopmomoa@users.noreply.github.com>
2023-07-22 22:09:22 -0700

    OBJECT FINDER WEB APP
    OBJECT FINDER WEB APP

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup(){
+    canvas = createCanvas(400, 400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size(400, 400);
+}
+
+function draw(){
+    image(video, 0, 0, 400, 400);
+}
