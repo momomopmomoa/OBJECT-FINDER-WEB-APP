2a5c5b10a525e5a8a1d49a76f938829fa33615c1
abhinav jha <116362054+momomopmomoa@users.noreply.github.com>
2023-07-25 21:46:46 -0700

    OBJECT FINDER WEB APP
    OBJECT FINDER WEB APP

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+status = "";
+
 function setup(){
     canvas = createCanvas(400, 400);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
     video.size(400, 400);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modalLoaded);
+    document.getElementById("input_1").value;
+}
+
+function modalLoaded(){
+    console.log("MODAL LOADED !")
+    status = true;
 }
 
 function draw(){
